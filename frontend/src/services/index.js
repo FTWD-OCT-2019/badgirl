045c77fdfb523b940d441c4bb212207383272147
Tzikas <niko.tzikas@gmail.com>
2019-12-09 18:07:52 -0500

    deploy

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 let baseURL;
 
 process.env.NODE_ENV === 'production'
-  ? (baseURL = 'here should be your production endpoint')
+  //? (baseURL = 'here should be your production endpoint')
+  ? (baseURL = window.location.origin)
   : (baseURL = 'http://localhost:3000');
 
 const service = axios.create({ withCredentials: true, baseURL });
