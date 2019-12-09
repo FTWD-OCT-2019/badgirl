1b304102c9c406f93b236c7cf37219725b5c528d
Tzikas <niko.tzikas@gmail.com>
2019-12-09 18:04:40 -0500

    deploy

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 const passport = require('./config/passport');
 
 mongoose
-  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch((err) => console.error('Error connecting to mongo', err));
 
